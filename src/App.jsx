e5c85c2d71fd68f65e35f81f119407070a2d0072
salmanulfarisvnb <salmanulfarisp9946@gmail.com>
2024-09-25 03:11:03 +0530

    api examble

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
+import React from "react";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [data, setData] = useState([]);
+
+  fetch("https://jsonplaceholder.typicode.com/photos")
+    .then((res) => res.json())
+    .then((data) => setData(data.splice(1, 10)))
+    .catch(() => {
+      console.log("error");
+    });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>ViteDeployTest</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {data.map((data, key) => (
+        <div key={key}>
+          <h1>{data.title}</h1>
+          <img src={data.thumbnailUrl} alt="" />
+        </div>
+      ))}
+    </div>
   );
-}
+};
 
 export default App;
